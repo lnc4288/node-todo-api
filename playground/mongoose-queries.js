df7ba60cdf1ccf4c2e31c7f96aaddac082a308cd
lnc4288 <lnc4288@outlook.com>
2018-02-27 13:48:25 -0700

    Add queries file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5a95bbcf205764c4313671401';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('Id not valid');
+// };
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('ID not found');
+//   }
+//   console.log('Todo By ID', todo);
+// }).catch((e) => console.log(e));
+
+var id = '5a94cbbc662ad8f808aae0bb';
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e)
+});
